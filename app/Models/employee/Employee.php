201c11cc250e7f0b763ee716589ae23f8519beca
Prince Bright <dankwaprince@gmail.com>
2023-07-18 16:34:26 +0000

    dan_update

--- app/Models/employee/Employee.php
+++ app/Models/employee/Employee.php
@@ -10,7 +10,37 @@ use Illuminate\Database\Eloquent\Model;
 class Employee extends Model
 {
     use HasFactory;
-    
+
+    public $fillable = [
+        'surname' ,
+            'other_names',
+            'staff_id',
+            'ssn_number',
+            'gender',
+            'country',
+            'd_o_b',
+            'marital_status_id',
+            'work_station',
+            'staff_status_id',
+            'division',
+            'department_id',
+            'job_title_id',
+            'job_category_id',
+            'first_app_date',
+            'qualification_id',
+            'starting_salary_id',
+            'monthly_salary',
+            'postal_address',
+            'city',
+            'city_region',
+            'home_towm',
+            'phone_number',
+            'email',
+            'home_town_region',
+            'avatar',
+            'residential_address',
+    ];
+
 
     public function staff_status(){
         return $this->hasOne(StaffStatus::class , 'staff_status_id');
